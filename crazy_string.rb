33ade618099268dd2986713793b0ee046809d5e3
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-13 22:26:01 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -6,7 +6,8 @@ def crazy_strings(a,b)
   #
   #  puts "#{a}.reverse.upcase" + "#{b}.gsub("s", "z")"
 
-   a.reverse.upcase + b.swapcase.gsub('s', 'z')
+   a.reverse.upcase 
+ b.swapcase.gsub('s', 'z')
 
     # puts "#{a}"+ "#{b}"
 end
