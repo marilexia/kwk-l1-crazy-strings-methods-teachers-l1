eabf704d6013450f106b74c399b20011aa6d8f61
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-13 22:20:06 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,8 +1,8 @@
 # write your method here
-a = gets.chomp
-b = gets.chomp
 
 def crazy_strings(a,b)
+  a = gets.chomp
+  b = gets.chomp
 
    puts "#{a}.reverse.upcase" + "#{b}.gsub("s", "z")"
   return
